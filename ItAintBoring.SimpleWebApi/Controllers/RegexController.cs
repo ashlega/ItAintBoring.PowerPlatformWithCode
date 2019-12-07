1473e5ae09cfe45e9a4e978be8fc916cd9be9c99
ashlega <ashlega@yahoo.com>
2019-12-06 23:07:03 -0500

    Regex webapi

---
+++ ItAintBoring.SimpleWebApi/Controllers/RegexController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItAintBoring.SimpleWebApi.Controllers
+{
+    public class RegexData
+    {
+        public string pattern { get; set; }
+        public string value { get; set; }
+    }
+
+    public class RegexResult
+    {
+        public string data { get; set; }
+        
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegexController : ControllerBase
+    {
+        
+        // POST api/regex
+        [HttpPost]
+        public ActionResult<RegexResult> Post([FromBody] RegexData data)
+        {
+            RegexResult result = new RegexResult();
+            MatchCollection foundMatches = Regex.Matches(data.value, data.pattern, RegexOptions.IgnoreCase);
+            foreach (Match m in foundMatches)
+            {
+                if (result.data != "") result.data += ";";
+                result.data += m.Value;
+            }
+            return result;
+        }
+
+        
+    }
+}
